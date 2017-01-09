af169000e9e589026fc000a0be0bcce2474abd3f
Michael Koltasheu <immerification@gmail.com>
2017-01-09 23:20:44 +0300

    Added manhattan distance visualization

--- src/app/board/models/board.ts
+++ src/app/board/models/board.ts
@@ -3,6 +3,27 @@ import { Cell } from './cell';
 
 export class Board {
 
+  get manhattanDistance(): number {
+    let distanceSum: number = 0;
+    this.randomlyPositionedTiles.forEach((tile) => {
+      let targetPositionX: number;
+      let targetPositionY: number;
+
+      let isTileMultipleDimension = tile.value % this.dimension === 0;
+      if (isTileMultipleDimension) {
+        targetPositionX = tile.value / this.dimension;
+        targetPositionY = this.dimension;
+      } else {
+        targetPositionY = tile.value % this.dimension;
+        targetPositionX = ((tile.value - targetPositionY) / this.dimension) + 1;
+      }
+      let manhattanDistanceForTile = Math.abs(tile.positionX - targetPositionX) + Math.abs(tile.positionY - targetPositionY);
+      tile.md = manhattanDistanceForTile;
+      distanceSum += manhattanDistanceForTile;
+    });
+
+    return distanceSum;
+  }
   constructor(
     public dimension: number,
     public randomlyPositionedTiles: Tile[],
