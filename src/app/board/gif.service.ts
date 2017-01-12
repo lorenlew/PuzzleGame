ae3afea764bd28b8b985dc31d9e122df87a4e59d
Michael Koltasheu <immerification@gmail.com>
2017-01-13 02:09:20 +0300

    added missed refs

--- src/app/board/gif.service.ts
+++ src/app/board/gif.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
 import { Observable } from 'rxjs/Observable';
 import GiphyData = Giphy.Data;
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
 
 @Injectable()
 export class GifService {
